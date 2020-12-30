001f53589e6385f6ec7cb763c58af37d3194f6e6
Felix Herbst <herbst@prefrontalcortex.de>
2020-12-30 23:39:54 +0100

    fix namespace

--- Editor/ShaderGraphPropertyUI.cs
+++ Editor/ShaderGraphPropertyUI.cs
@@ -1,76 +1,78 @@
-using Needle;
 using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEditor;
 using UnityEditor.Experimental.GraphView;
 using System.Reflection;
 
-public static class ShaderGraphPropertyUI
+namespace Needle.ShaderGraphMarkdown
 {
-    [InitializeOnLoadMethod]
-    static void Init()
+    public static class ShaderGraphPropertyUI
     {
-        EditorApplication.update += EditorUpdate;
-    }
-
-    private static int i = 0;
-    private static StyleSheet styleSheet;
-    
-    private static void EditorUpdate()
-    {
-        i = (i + 1) % 100; // only run every 100 frames for now
-        if (i != 0) return;
-
-        #if SHADERGRAPH_10_OR_NEWER
-        var shaderGraphAssembly = typeof(UnityEditor.ShaderGraph.PositionControl).Assembly; // does not exist earlier
-        #else
-        var shaderGraphAssembly = typeof(UnityEditor.ShaderGraph.KeywordDefinition).Assembly; // is internal in 2020.2+
-        #endif
-        var windowType = shaderGraphAssembly.GetType("UnityEditor.ShaderGraph.Drawing.MaterialGraphEditWindow");
-        var windows = Resources.FindObjectsOfTypeAll(windowType);
-        foreach (var wnd in windows)
+        [InitializeOnLoadMethod]
+        static void Init()
         {
-            var _graphEditorView = windowType.GetProperty("graphEditorView", (BindingFlags) (-1));
-            var graphEditorView = _graphEditorView?.GetValue(wnd);
-            if(graphEditorView == null) continue;
+            EditorApplication.update += EditorUpdate;
+        }
 
-            var GraphEditorView = graphEditorView.GetType();
+        private static int i = 0;
+        private static StyleSheet styleSheet;
+        
+        private static void EditorUpdate()
+        {
+            i = (i + 1) % 100; // only run every 100 frames for now
+            if (i != 0) return;
 
-            var _blackboardProvider = GraphEditorView.GetProperty("blackboardProvider", (BindingFlags) (-1));
-            var blackboardProvider = _blackboardProvider?.GetValue(graphEditorView);
-            if(blackboardProvider == null) continue;
-
-            var BlackboardProvider = blackboardProvider.GetType();
-
-            var _blackboard = BlackboardProvider.GetProperty("blackboard", (BindingFlags) (-1));
-            var blackboard = (Blackboard) _blackboard?.GetValue(blackboardProvider);
-            if(blackboard == null) continue;
-            
-            if (!styleSheet)
-                styleSheet = Resources.Load<StyleSheet>("Styles/ShaderGraphMarkdown");
-            
-            var blackboardElements = blackboard.Query<BlackboardField>().Visible().ToList();
-            foreach(var fieldView in blackboardElements)
+            #if SHADERGRAPH_10_OR_NEWER
+            var shaderGraphAssembly = typeof(UnityEditor.ShaderGraph.PositionControl).Assembly; // does not exist earlier
+            #else
+            var shaderGraphAssembly = typeof(UnityEditor.ShaderGraph.KeywordDefinition).Assembly; // is internal in 2020.2+
+            #endif
+            var windowType = shaderGraphAssembly.GetType("UnityEditor.ShaderGraph.Drawing.MaterialGraphEditWindow");
+            var windows = Resources.FindObjectsOfTypeAll(windowType);
+            foreach (var wnd in windows)
             {
-                var displayName = fieldView.text;
-                    
-                var markdownType = MarkdownShaderGUI.GetMarkdownType(displayName);
-                switch (markdownType)
-                {
-                    case MarkdownShaderGUI.MarkdownProperty.None:
-                        break;
-                    default:
-                        if (!fieldView.styleSheets.Contains(styleSheet))
-                            fieldView.styleSheets.Add(styleSheet);
+                var _graphEditorView = windowType.GetProperty("graphEditorView", (BindingFlags) (-1));
+                var graphEditorView = _graphEditorView?.GetValue(wnd);
+                if(graphEditorView == null) continue;
+
+                var GraphEditorView = graphEditorView.GetType();
+
+                var _blackboardProvider = GraphEditorView.GetProperty("blackboardProvider", (BindingFlags) (-1));
+                var blackboardProvider = _blackboardProvider?.GetValue(graphEditorView);
+                if(blackboardProvider == null) continue;
+
+                var BlackboardProvider = blackboardProvider.GetType();
+
+                var _blackboard = BlackboardProvider.GetProperty("blackboard", (BindingFlags) (-1));
+                var blackboard = (Blackboard) _blackboard?.GetValue(blackboardProvider);
+                if(blackboard == null) continue;
                 
-                        if (!fieldView.ClassListContains("__markdown"))
-                            fieldView.AddToClassList("__markdown");
-                        var contentItem = fieldView.Q("contentItem");
-                        contentItem.ClearClassList();
-                        contentItem.AddToClassList("__markdown_" + markdownType);
+                if (!styleSheet)
+                    styleSheet = Resources.Load<StyleSheet>("Styles/ShaderGraphMarkdown");
+                
+                var blackboardElements = blackboard.Query<BlackboardField>().Visible().ToList();
+                foreach(var fieldView in blackboardElements)
+                {
+                    var displayName = fieldView.text;
                         
-                        fieldView.typeText = markdownType.ToString();
-                        break;
+                    var markdownType = MarkdownShaderGUI.GetMarkdownType(displayName);
+                    switch (markdownType)
+                    {
+                        case MarkdownShaderGUI.MarkdownProperty.None:
+                            break;
+                        default:
+                            if (!fieldView.styleSheets.Contains(styleSheet))
+                                fieldView.styleSheets.Add(styleSheet);
+                    
+                            if (!fieldView.ClassListContains("__markdown"))
+                                fieldView.AddToClassList("__markdown");
+                            var contentItem = fieldView.Q("contentItem");
+                            contentItem.ClearClassList();
+                            contentItem.AddToClassList("__markdown_" + markdownType);
+                            
+                            fieldView.typeText = markdownType.ToString();
+                            break;
+                    }
                 }
             }
         }
