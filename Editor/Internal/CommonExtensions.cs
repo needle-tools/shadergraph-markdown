cc554530bacf30f049695ffee953ca7ebf09ca69
Felix Herbst <herbst@prefrontalcortex.de>
2021-01-04 00:01:25 +0100

    add script file to prevent warnings on 2021 for empty asmdef

---
+++ Editor/Internal/CommonExtensions.cs
@@ -0,0 +1,10 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+//
+// namespace Needle.ShaderGraphMarkdown
+// {
+//     public static class CommonExtensions
+//     {
+//     }
+// }
